a38b93de427ec43fd9bdb0bc5a02c2c7a0f7fcef
Christine Chee <christinechee2000@gmail.com>
2025-12-07 00:15:47 -0500

    total budget charts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,25 @@
+// === Income ===
+const openAddIncome = document.getElementById('open-add-income');
+const closeAddIncome = document.getElementById('close-add-income');
+const editIncome = document.getElementById('edit-income');
+const openIncomeDetail = document.getElementById('income-details');
+const selectIncomeCategory = document.getElementById("select-income-category");
+const customIncomeContainer = document.querySelector(".custom-income-container"); // Called here to unhide container
+const updateIncomeTable = document.getElementById('add-income');
 
-document.addEventListener("DOMContentLoaded", () => {
-    // === Income ===
-    const openAddIncome = document.getElementById('open-add-income');
-    const closeAddIncome = document.getElementById('close-add-income');
-    const editIncome = document.getElementById('edit-income');
-    const openIncomeDetail = document.getElementById('income-details');
-    const selectIncomeCategory = document.getElementById("select-income-category");
-    const customIncomeContainer = document.querySelector(".custom-income-container"); // Called here to unhide container
-    const updateIncomeTable = document.getElementById('add-income');
+// === Expenses ===
+const openAddExpenses = document.getElementById('open-add-expenses');
+const closeAddExpenses = document.getElementById('close-add-expenses');
+const editExpenses = document.getElementById('edit-expenses');
+const openExpensesDetails = document.getElementById('expenses-details');
+const selectExpensesCategory = document.getElementById("select-expenses-category");
+const customExpensesContainer = document.querySelector(".custom-expenses-container"); // Called here to unhide container
+const updateExpensesTable = document.getElementById('add-expenses');
 
-    // === Expenses ===
-    const openAddExpenses = document.getElementById('open-add-expenses');
-    const closeAddExpenses = document.getElementById('close-add-expenses');
-    const editExpenses = document.getElementById('edit-expenses');
-    const openExpensesDetails = document.getElementById('expenses-details');
-    const selectExpensesCategory = document.getElementById("select-expenses-category");
-    const customExpensesContainer = document.querySelector(".custom-expenses-container"); // Called here to unhide container
-    const updateExpensesTable = document.getElementById('add-expenses');
+// == Total Budget ==
+let totalComparisonChart;
+let budgetBreakdownChart;
 
-
-    const calculateBudget = new Budget();
-
-    // [ Load Categories from JSON]
-    // Load categories from JSON
-    fetch('assets/data/categories.json')
-    .then(response => response.json())
-    .then(data => {
-        loadSelectOptions('select-income-category', data.income);
-        loadSelectOptions('select-expenses-category', data.expenses);
-    })
-    .catch(err => console.error('Error loading categories.json:', err));
-
-    function loadSelectOptions(selectId, items) {
-        const select = document.getElementById(selectId);
-        select.innerHTML = ""; // clear existing options
-
-        items.forEach(item => {
-            const option = document.createElement('option');
-            option.value = item;
-            option.textContent = item;
-            select.appendChild(option);
-        });
-    }
-
-
-
-    // [ -- ADD INCOME / EXPENSE BUTTON -- ]
-    // When Add is clicked, display hiddden section.
-    openAddIncome.addEventListener('click', (event) => {
-        editIncome.style.display = 'flex'; // Change the edit container back to visible
-        openIncomeDetail.open = true; // Opens the closed details section
-    });
-
-    openAddExpenses.addEventListener('click', (event) => {
-        editExpenses.style.display = 'flex';
-        openExpensesDetails.open = true;
-    });
-
-    // [ -- CANCEL INCOME / EXPENSE BUTTON -- ]
-    closeAddIncome.addEventListener('click', (event) => {
-        editIncome.style.display = 'none'; // Change the edit container back to none
-    });
-
-    closeAddExpenses.addEventListener('click', (event) => {
-        editExpenses.style.display = 'none'; 
-    });
-
-    // [ -- Custom Category Section --]
-    // When Custom category is selcted, open custom field
-    selectIncomeCategory.addEventListener('change', () => {
-        // If the value selected is custom(the value), display hidden custom section
-        if(selectIncomeCategory.value === "custom") {
-            customIncomeContainer.style.display = 'flex';
-        }
-        // Else, leave section hidden
-        else {
-            customIncomeContainer.style.display = 'none';
-        }
-    });
-
-    selectExpensesCategory.addEventListener('change', () => {
-        if(selectExpensesCategory.value === "custom") {
-            customExpensesContainer.style.display = 'flex';
-        }
-        else {
-            customExpensesContainer.style.display = 'none';
-        }
-    });
-
-    // [ ADD INCOME ]
-    updateIncomeTable.addEventListener('click', (event) => {
-        // Prevent pagge from reloading to defualt, otherwise totals/ew inputs are not saved.
-        event.preventDefault();
-        calculateBudget.addIncome();
-    });
-
-    // [ ADD Expenses ]
-    updateExpensesTable.addEventListener('click', (event) => {
-        // Prevent pagge from reloading to defualt, otherwise total inputs are not saved.
-        event.preventDefault();
-        calculateBudget.addExpenses();
-    });
-
-});
 
 // === Budget Calculation ===
 /**
@@ -118,6 +35,7 @@ class Budget {
 
     /**
      * Updates either Income or Expenses Table and Total.
+     * Updates charts
      * 
      * Takes in a string parameter catergoryType to know which table to update.
      * Will come from either addIncome(income) or addExpenses(expenses)
@@ -212,6 +130,9 @@ class Budget {
         // Call update to add last row & update total
         this.update('income');
 
+        // Update charts
+        this.updateCharts();
+
         // Reset form
         categorySelection.value = 'Paycheck';
         customCategoryInput.value = '';
@@ -256,6 +177,9 @@ class Budget {
         // Call update to add last row & update total
         this.update('expenses');
 
+        // Update charts
+        this.updateCharts();
+
         // Reset form
         categorySelection.value = 'Food'; // First option
         customCategoryInput.value = '';
@@ -264,7 +188,89 @@ class Budget {
         expenseErrorMessaage.style.display = "none";
     }
 
-    // -- Budget Plan Selection --
+    // -- Budget Chart --
+    updateCharts() {
+        /**
+         * reduce() adds the sum of items with the value starting at 0
+         * will loop through every item in each array
+         */
+        const incomeTotal = this.income.reduce((sum, item) => sum + item.amount, 0);
+        const expensesTotal = this.expenses.reduce((sum, item) => sum + item.amount, 0);
+
+        // --- Income vs Expenses BAR CHART ---
+        const barChart = document.getElementById('total-comparison-chart'); //caanvas element
+        
+        if (barChart) {
+            const barContext = barChart.getContext('2d'); //getss 2d rednderinigg from Charts.js
+            if (totalComparisonChart) totalComparisonChart.destroy(); // redraws chart of it alreaddy exists
+
+            // Create a new bar chart
+            totalComparisonChart = new Chart(barContext, {
+                type: 'bar',
+                data: {
+                    labels: ['Income', 'Expenses'], // x labels
+                    datasets: [{
+                        label: 'Amount ($)',
+                        data: [incomeTotal, expensesTotal], // y values
+                        backgroundColor: ['#4caf50', '#f44336']
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        y: { beginAtZero: true } // charts always starts at 0
+                    }
+                }
+            });
+        }
+
+        // --- BUDGET BREAKDOWN ---
+        // Rule: 50 Needs/ 30 Wants/ 20 Savings
+
+        const pieChart = document.getElementById('budget-breakdown-chart');
+
+        if (pieChart) {
+            const pieContext = pieChart.getContext('2d'); // Grabs 2 render
+            if (budgetBreakdownChart) budgetBreakdownChart.destroy(); // Breakdown old chart if it exists
+
+            const needs = incomeTotal * 0.50 ; // 50%
+            const wants = incomeTotal * 0.30; // 30%
+            const savings = incomeTotal * 0.20; // 20%
+
+            // --- Handles initial empty data ---
+            let pieData, pieLabels, pieColors;
+
+            // if income is 0
+            if (incomeTotal === 0) {
+                pieData = [1]; // initial slice
+                pieLabels = ['No Data'];
+                pieColors = ['#cccccc']; // empty color
+            } 
+            // else load/update in data an labels
+            else {
+                pieData = [incomeTotal * 0.5, incomeTotal * 0.3, incomeTotal * 0.2];
+                pieLabels = ['Needs (50%)', 'Wants (30%)', 'Savings (20%)'];
+                pieColors = ['#2196f3', '#ff9800', '#9c27b0'];
+            }
+
+            // Create Pie Chart
+            budgetBreakdownChart = new Chart(pieContext, {
+                type: 'pie',
+                data: {
+                    labels: pieLabels,
+                    datasets: [{
+                        data: pieData,
+                        backgroundColor: pieColors
+                    }]
+                },
+                options: {
+                    responsive: true
+                }
+            });
+        }
+    }
+
+    // -- Budget Plan Selection (If time allows)--
     /**
      * 50 Needs/ 30 Wants/ 20 Savings & Debt
      * 50 Needs/ 40 Debt/ 10 Wants & Savings
@@ -274,4 +280,91 @@ class Budget {
 
 
 }
+
+const calculateBudget = new Budget();
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // [ Load Categories from JSON]
+    // Load categories from JSON
+    fetch('assets/data/categories.json')
+    .then(response => response.json())
+    .then(data => {
+        loadSelectOptions('select-income-category', data.income);
+        loadSelectOptions('select-expenses-category', data.expenses);
+    })
+    .catch(err => console.error('Error loading categories.json:', err));
+
+    function loadSelectOptions(selectId, items) {
+        const select = document.getElementById(selectId);
+        select.innerHTML = ""; // clear existing options
+
+        items.forEach(item => {
+            const option = document.createElement('option');
+            option.value = item;
+            option.textContent = item;
+            select.appendChild(option);
+        });
+    }
+
+    calculateBudget.updateCharts();
+
+    // [ -- ADD INCOME / EXPENSE BUTTON -- ]
+    // When Add is clicked, display hiddden section.
+    openAddIncome.addEventListener('click', (event) => {
+        editIncome.style.display = 'flex'; // Change the edit container back to visible
+        openIncomeDetail.open = true; // Opens the closed details section
+    });
+
+    openAddExpenses.addEventListener('click', (event) => {
+        editExpenses.style.display = 'flex';
+        openExpensesDetails.open = true;
+    });
+
+    // [ -- CANCEL INCOME / EXPENSE BUTTON -- ]
+    closeAddIncome.addEventListener('click', (event) => {
+        editIncome.style.display = 'none'; // Change the edit container back to none
+    });
+
+    closeAddExpenses.addEventListener('click', (event) => {
+        editExpenses.style.display = 'none'; 
+    });
+
+    // [ -- Custom Category Section --]
+    // When Custom category is selcted, open custom field
+    selectIncomeCategory.addEventListener('change', () => {
+        // If the value selected is custom(the value), display hidden custom section
+        if(selectIncomeCategory.value === "custom") {
+            customIncomeContainer.style.display = 'flex';
+        }
+        // Else, leave section hidden
+        else {
+            customIncomeContainer.style.display = 'none';
+        }
+    });
+
+    selectExpensesCategory.addEventListener('change', () => {
+        if(selectExpensesCategory.value === "custom") {
+            customExpensesContainer.style.display = 'flex';
+        }
+        else {
+            customExpensesContainer.style.display = 'none';
+        }
+    });
+
+    // [ ADD INCOME ]
+    updateIncomeTable.addEventListener('click', (event) => {
+        // Prevent pagge from reloading to defualt, otherwise totals/ew inputs are not saved.
+        event.preventDefault();
+        calculateBudget.addIncome();
+    });
+
+    // [ ADD Expenses ]
+    updateExpensesTable.addEventListener('click', (event) => {
+        // Prevent pagge from reloading to defualt, otherwise total inputs are not saved.
+        event.preventDefault();
+        calculateBudget.addExpenses();
+    });
+
+});
 
